85f0eacb1cf3a388b70518b8eae81717828a5597
Jonathan Commins <joncom@gmail.com>
2013-06-13 14:41:26 -0700

    Added binding for new struct.

--- embindings.cpp
+++ embindings.cpp
@@ -6,4 +6,8 @@ using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(my_module) {
     function("lerp", &lerp);
+    value_struct<myStruct>("myStruct")
+        .field("x", &myStruct::x)
+        .field("y", &myStruct::y)
+        ;
 }
